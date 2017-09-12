1242de2bd1dcdc119d4d92cf5f4a0f3b786f4d34
Masashi Okita <easychoco2@gmail.com>
2017-09-12 13:47:08 +0900

    commit

--- src/StateNS/GameNS/GameMainNS/GameMain/Player/Nue.h
+++ src/StateNS/GameNS/GameMainNS/GameMain/Player/Nue.h
@@ -5,48 +5,48 @@
 
 
 namespace StateNS {
-	namespace GameNS {
-		namespace GameMainNS {
+namespace GameNS {
+namespace GameMainNS {
 
 
-			class Nue : public PlayerChild
-			{
-			public:
-				Nue(int x, int y, int hp);
-				Nue(int, int);
-				~Nue();
-				PlayerChild* update(const Stage*);
+class Nue : public PlayerChild
+{
+public:
+	Nue(int x, int y, int hp);
+	Nue(int, int);
+	~Nue();
+	PlayerChild* update(const Stage*);
 
-			private:
-				int attackTime;
-				const int attackInterval = 10;//UŒ‚‚ÌŠÔŠu
+private:
+	int attackTime;
+	const int attackInterval = 10;//UŒ‚‚ÌŠÔŠu
 
-				void initialize();
-				void attack();
-				void draw_other() const;
-				virtual void loadImage() override;
+	void initialize();
+	void attack();
+	void draw_other() const;
+	virtual void loadImage() override;
 
-				virtual int specialAction()override;
+	virtual int specialAction() override;
 
-				//UŒ‚•û–@‚Ì“à•”ƒNƒ‰ƒX
-				class Spear : public Attack
-				{
-				public:
-					Spear(int x, int y, int w, int h, bool direction);
-					~Spear();
-					void update();
-					void setStatus(Vector2, int dx);
-					void hittedAction();
+	//UŒ‚•û–@‚Ì“à•”ƒNƒ‰ƒX
+	class Spear : public Attack
+	{
+	public:
+		Spear(int x, int y, int w, int h, bool direction);
+		~Spear();
+		void update();
+		void setStatus(Vector2, int dx);
+		void hittedAction();
 
-				private:
-					int mTime;
+	private:
+		int mTime;
 
-				};
-				bool isUFO;
-				UFO* ufo;
-			};
+	};
+	bool isUFO;
+	UFO* ufo;
+};
 
 
-		}
-	}
+}
+}
 }
