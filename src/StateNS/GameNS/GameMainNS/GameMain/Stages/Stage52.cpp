f3029926cd3d9aa7f43fa8af43db5fb6ff840a7d
Masashi Okita <easychoco2@gmail.com>
2017-10-13 21:28:42 +0900

    merge

--- src/StateNS/GameNS/GameMainNS/GameMain/Stages/Stage52.cpp
+++ src/StateNS/GameNS/GameMainNS/GameMain/Stages/Stage52.cpp
@@ -36,15 +36,11 @@ void Stage52::initialize()
 	this->maps.push_back(new Map(52, 3, MapPos::POS_LEFT_DOWN));
 	this->maps.push_back(new Map(52, 4, MapPos::POS_UP_DOWN));
 	this->maps.push_back(new Map(52, 5, MapPos::POS_RIGHT_DOWN));
-<<<<<<< HEAD
-
 
 	flag = new ClearFlag(Vector2(97 * 32 + 16, 6 * 32 + 16));
 	maps[0]->addGimmick(flag);
 	now_stage_num = 1;
 	startX = 144, startY = 1536;
-=======
->>>>>>> origin/master
 
 	SwitchWithBlock* s = new SwitchWithBlock(91 * 32 + 16, 12 * 32 + 16, 1);
 	for (int i = 0; i < 6; i++)s->push_block(new Block((92 + i) * 32 + 16, 13 * 32 + 16, 1.0, BlockType::TYPE_SWITCH), false);
@@ -76,43 +72,6 @@ void Stage52::initialize()
 	for (int i = 0; i < 4; i++)s8->push_block(new Block(87 * 32 + 16, (28 + i) * 32 + 16, 1.0, BlockType::TYPE_SWITCH), false);
 	maps[3]->addSwitchWithBlock(s8);
 
-<<<<<<< HEAD
-=======
-	flag = new ClearFlag(Vector2(97 * 32 + 16, 6 * 32 + 16));
-	maps[0]->addGimmick(flag);
-	now_stage_num = 1;
-	startX = 144, startY = 1536;
-	
-	SwitchWithBlock* s = new SwitchWithBlock(91 * 32 + 16, 12 * 32 + 16, 1);
-	for (int i = 0; i < 6; i++)s->push_block(new Block((92 + i) * 32 + 16, 13 * 32 + 16, 1.0, BlockType::TYPE_SWITCH), false);
-	maps[2]->addSwitchWithBlock(s);
-	SwitchWithBlock* s1 = new SwitchWithBlock(98 * 32 + 16, 12 * 32 + 16, 1);
-	for (int i = 0; i < 6; i++)s1->push_block(new Block((92 + i) * 32 + 16, 13 * 32 + 16, 1.0, BlockType::TYPE_SWITCH), false);
-	maps[2]->addSwitchWithBlock(s1);
-
-	SwitchWithBlock* s2 = new SwitchWithBlock(66 * 32 + 16, 20 * 32 + 16, 99);
-	for (int i = 0; i < 3; i++)s2->push_block(new Block((63 + i) * 32 + 16, 20 * 32 + 16, 1.0, BlockType::TYPE_SWITCH), true);
-	maps[3]->addSwitchWithBlock(s2);
-	SwitchWithBlock* s3 = new SwitchWithBlock(58 * 32 + 16, 23 * 32 + 16, 99);
-	for (int i = 0; i < 3; i++)s3->push_block(new Block((59 + i) * 32 + 16, 23 * 32 + 16, 1.0, BlockType::TYPE_SWITCH), true);
-	maps[3]->addSwitchWithBlock(s3);
-	SwitchWithBlock* s4 = new SwitchWithBlock(66 * 32 + 16, 26 * 32 + 16, 99);
-	for (int i = 0; i < 3; i++)s4->push_block(new Block((63 + i) * 32 + 16, 26 * 32 + 16, 1.0, BlockType::TYPE_SWITCH), true);
-	maps[3]->addSwitchWithBlock(s4);
-	SwitchWithBlock* s5 = new SwitchWithBlock(58 * 32 + 16, 29 * 32 + 16, 99);
-	for (int i = 0; i < 3; i++)s5->push_block(new Block((59 + i) * 32 + 16, 29 * 32 + 16, 1.0, BlockType::TYPE_SWITCH), true);
-	maps[3]->addSwitchWithBlock(s5);
-
-	SwitchWithBlock* s6 = new SwitchWithBlock(80 * 32 + 16, 28 * 32 + 16, 99);
-	for (int i = 0; i < 4; i++)s6->push_block(new Block(77 * 32 + 16, (28 + i) * 32 + 16, 1.0, BlockType::TYPE_SWITCH), false);
-	maps[3]->addSwitchWithBlock(s6);
-	SwitchWithBlock* s7 = new SwitchWithBlock(80 * 32 + 16, 31 * 32 + 16, 99);
-	for (int i = 0; i < 4; i++)s7->push_block(new Block(77 * 32 + 16, (28 + i) * 32 + 16, 1.0, BlockType::TYPE_SWITCH), false);
-	maps[3]->addSwitchWithBlock(s7);
-	SwitchWithBlock* s8 = new SwitchWithBlock(92 * 32 + 16, 31 * 32 + 16, 99);
-	for (int i = 0; i < 4; i++)s8->push_block(new Block(87 * 32 + 16, (28 + i) * 32 + 16, 1.0, BlockType::TYPE_SWITCH), false);
-	maps[3]->addSwitchWithBlock(s8);
->>>>>>> origin/master
 	//for Debug
 	/*
 	maps[0]->addEnemy(AllEnemies::BOSS_REISEN, 640, 1392);
